c05612d679ea061f6526e3545b156b79fe6e38e5
Andrés Gutiérrez <andgr1987@hotmail.com>
2026-03-01 14:20:28 -0600

    Commit API

--- Vehiculo.API/Abstracciones/Modelos/Marca.cs
+++ Vehiculo.API/Abstracciones/Modelos/Marca.cs
@@ -4,7 +4,7 @@ namespace Abstracciones.Modelos
 {
     public class MarcaBase
     {
-        [Required(ErrorMessage = "La propiedad nombre es requerida")]
+        [Required(ErrorMessage = "La propiedad nombre es obligatoria")]
         [StringLength(80, ErrorMessage = "La propiedad nombre debe ser mayor a 2 caracteres y menor a 80", MinimumLength = 2)]
         public string Nombre { get; set; }
     }
